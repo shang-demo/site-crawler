2170d572cb74fd647e4dd0c4cf42497439643cf8
xinshangshangxin <xinshangshangxin@gmail.com>
2017-04-29 00:41:15 +0800

    refactor: update record

--- src/app/site-setting/site-setting.component.ts
+++ src/app/site-setting/site-setting.component.ts
@@ -10,7 +10,19 @@ import { SiteService } from '../site/site.service';
 })
 
 export class SiteSettingComponent implements OnInit {
-  public sites = [];
+  public sites = [{
+    site: 'zd',
+    description: '专注绿软，分享软件、传递最新软件资讯',
+    updatedAt: null,
+  }, {
+    site: 'llm',
+    description: '浏览迷(原浏览器之家)是一个关注浏览器及软件、IT的科技博客,致力于为广大浏览器爱好者提供一个关注浏览器、交流浏览器、折腾浏览器的专门网站',
+    updatedAt: null,
+  }, {
+    site: 'xclient',
+    description: '爱Q生活网 - 亮亮\'blog -关注最新QQ活动动态, 掌握QQ第一资讯',
+    updatedAt: null,
+  }];
 
   constructor(public dialogRef: MdDialogRef<SiteSettingComponent>,
               public siteService: SiteService) {
@@ -19,11 +31,18 @@ export class SiteSettingComponent implements OnInit {
   }
 
   public ngOnInit(): void {
+    this.sites.forEach((site) => {
+      delete site.updatedAt;
+    });
+
     this.siteService.sites.forEach((name) => {
       this.siteService.crawlerRecord(name)
         .subscribe((result) => {
-          console.info('result: ', result);
-          this.sites.push(result);
+          this.sites.forEach((site) => {
+            if (site.site === result.site) {
+              site.updatedAt = result.updatedAt;
+            }
+          });
           console.info('this.sites: ', this.sites);
         });
     });
